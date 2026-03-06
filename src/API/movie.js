94ae03d4160195e539bac8ac6ec40a82aa6fd8bb
GAMMJ <gammjmj@gmail.com>
2026-03-06 16:08:01 +0900

    feat: API 영화 데이터 받아오기 구현

---
+++ src/API/movie.js
@@ -0,0 +1,21 @@
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+  },
+}
+
+export const getPopularMovies = async () => {
+  try {
+    const response = await fetch("https://api.themoviedb.org/3/movie/popular", options)
+    if (!response.ok) {
+      throw new Error("네트워크 에러 발생")
+    }
+    const data = await response.json()
+    const filteredData = data.results.filter((movie) => movie.adult === false)
+    return filteredData
+  } catch (error) {
+    console.error("데이터 받아오기 에러:", error)
+  }
+}
