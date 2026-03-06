ae1f8820f70fa11e6af8c5197e409ae6c0582a83
GAMMJ <gammjmj@gmail.com>
2026-03-06 16:09:19 +0900

    feat: 인기 영화 데이터 메인페이지 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,38 @@
+import { useState, useEffect } from "react"
 import MovieCard from "../components/MovieCard"
-import movieListData from "../data/movieListData.json"
 import SwiperMovie from "../util/SwiperMovie.jsx"
+import { getPopularMovies } from "../API/movie"
 
 const Home = () => {
+  const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const data = await getPopularMovies()
+        setMovies(data)
+      } catch (error) {
+        console.error("데이터 로딩 실패:", error)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchMovies()
+  }, [])
+
+  if (loading) {
+    return <div className="text-white text-center mt-20">로딩 중...</div>
+  }
+
   return (
     <>
       <section className="mb-8 relative">
-        <SwiperMovie movies={movieListData.results} />
+        <SwiperMovie movies={movies} />
       </section>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
-        {movieListData.results.map((movie) => (
+        {movies.map((movie) => (
           <MovieCard key={movie.id} movieData={movie} />
         ))}
       </div>
