465d0b3c9761ad8a254532dbb3708a336706ea16
GAMMJ <gammjmj@gmail.com>
2026-03-08 16:20:40 +0900

    refactor: swiper css 파일 분리

--- src/components/ui/SwiperMovie.jsx
+++ src/components/ui/SwiperMovie.jsx
@@ -1,3 +1,4 @@
+import "./SwiperMovie.css"
 import { Swiper, SwiperSlide } from "swiper/react"
 
 // Swiper 스타일 (필수)
@@ -12,17 +13,18 @@ const SwiperMovie = ({ children }) => {
   return (
     <>
       <Swiper
-        // 사용할 모듈 등록
+        className="movie-swiper"
         modules={[Navigation, Autoplay]}
-        spaceBetween={20} // 슬라이드 간 간격
-        slidesPerView={5} // 한 번에 보여줄 슬라이드 개수
+        spaceBetween={20}
+        slidesPerView={5}
         navigation={{
           nextEl: ".btn-next",
           prevEl: ".btn-prev",
-        }} // 화살표 네비게이션 사용
-        autoplay={{ delay: 3000 }} // 자동 재생 (선택사항)
+        }}
+        autoplay={{ delay: 3000 }}
+        centeredSlides={true}
+        loop={true}
         breakpoints={{
-          // 반응형 설정
           320: { slidesPerView: 1 },
           768: { slidesPerView: 3 },
           1024: { slidesPerView: 5 },
@@ -33,7 +35,6 @@ const SwiperMovie = ({ children }) => {
       <button className="btn-prev absolute left-2 top-1/2 -translate-y-1/2 z-10 bg-black/50 text-white w-8 h-8 rounded-full">
         ‹
       </button>
-
       <button className="btn-next absolute right-2 top-1/2 -translate-y-1/2 z-10 bg-black/50 text-white w-8 h-8 rounded-full">
         ›
       </button>
