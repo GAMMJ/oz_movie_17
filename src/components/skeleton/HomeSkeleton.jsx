e5569bc8381a03a9caa0c03fed785ea143363618
GAMMJ <gammjmj@gmail.com>
2026-03-10 16:08:51 +0900

    refactor: 스켈레톤 수정

--- src/components/skeleton/HomeSkeleton.jsx
+++ src/components/skeleton/HomeSkeleton.jsx
@@ -15,12 +15,15 @@ const HomeSkeleton = () => (
     </section>
 
     {/* 그리드 스켈레톤 */}
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8">
       {Array.from({ length: 10 }).map((_, i) => (
-        <div key={i} className="animate-pulse">
-          <div className="aspect-2/3 bg-gray-700 rounded-lg mb-2" />
-          <div className="h-4 bg-gray-700 rounded w-3/4 mb-1" />
-          <div className="h-3 bg-gray-600 rounded w-1/2" />
+        <div key={i} className="flex flex-col bg-[#202020] rounded-xl overflow-hidden">
+          <div className="aspect-2/3 bg-gray-700 animate-pulse" />
+
+          <div className="p-4 flex flex-col grow">
+            <div className="h-4 bg-gray-700 rounded w-3/4 mb-2 min-h-12 animate-pulse" />
+            <div className="h-3 bg-gray-600 rounded w-1/2 animate-pulse" />
+          </div>
         </div>
       ))}
     </div>
